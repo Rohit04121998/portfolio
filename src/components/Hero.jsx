318f9de59b615382317af638aabd3cede75cef19
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-25 01:21:05 -0700

    adjusted margin top

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const container = (delay) => ({
 
 const Hero = () => {
   return (
-    <div className="lg:mb-35 mt-40 pb-4" id="hero">
+    <div className="mt-35 pb-4 lg:mb-1" id="hero">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
