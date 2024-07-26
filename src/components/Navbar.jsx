1b487c14bc4c27e73f4c530e00b4657d73655e32
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-25 17:52:32 -0700

    chore: Update Navbar styling and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
       duration: 2000,
       delay: 0,
       smooth: "easeInOutQuart",
-      offset: -70,
+      offset: -120,
     });
   };
 
   return (
-    <nav className="navbar mb-20 flex w-full items-center justify-between bg-black bg-opacity-95 py-6">
-      <img src={logo} alt="hoobank" className="w-[60px]" />
+    <nav className="navbar fixed left-0 right-0 top-0 z-50 mx-4 flex items-center justify-between bg-black bg-opacity-80 py-6">
+      <img src={logo} alt="hoobank" className="w-[60px] lg:ml-16" />
 
       <ul className="hidden flex-1 list-none items-center justify-end sm:hidden md:hidden lg:flex">
         {navLinks.map((nav, index) => (
@@ -28,7 +28,7 @@ const Navbar = () => {
             key={nav.link}
             className={`font-poppins cursor-pointer text-[20px] font-normal ${
               active === nav.name ? "text-white" : "text-dimWhite"
-            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            } ${index === navLinks.length - 1 ? "mr-10" : "mr-10"}`}
             onClick={() => {
               setActive(nav.name);
               scrollWithOffset(nav.link);
@@ -50,7 +50,7 @@ const Navbar = () => {
         <div
           className={`${
             !toggle ? "hidden" : "flex"
-          } sidebar absolute right-0 top-20 mx-4 my-2 min-w-[170px] rounded-xl bg-black bg-opacity-95 p-6`}
+          } sidebar absolute right-0 top-full mx-4 my-2 min-w-[170px] rounded-xl bg-black bg-opacity-95 p-6`}
         >
           <ul className="flex flex-1 list-none flex-col items-start justify-end">
             {navLinks.map((nav, index) => (
@@ -68,6 +68,56 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
+
+    // <nav className="navbar mb-20 flex w-full items-center justify-between bg-black bg-opacity-95 py-6">
+    //   <img src={logo} alt="hoobank" className="w-[60px]" />
+
+    //   <ul className="hidden flex-1 list-none items-center justify-end sm:hidden md:hidden lg:flex">
+    //     {navLinks.map((nav, index) => (
+    //       <li
+    //         key={nav.link}
+    //         className={`font-poppins cursor-pointer text-[20px] font-normal ${
+    //           active === nav.name ? "text-white" : "text-dimWhite"
+    //         } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+    //         onClick={() => {
+    //           setActive(nav.name);
+    //           scrollWithOffset(nav.link);
+    //         }}
+    //       >
+    //         <a href={`#${nav.link}`}>{nav.name}</a>
+    //       </li>
+    //     ))}
+    //   </ul>
+
+    //   <div className="flex flex-1 cursor-pointer items-center justify-end sm:flex md:flex lg:hidden">
+    //     <img
+    //       src={toggle ? close : menu}
+    //       alt="menu"
+    //       className="h-[28px] w-[28px] object-contain"
+    //       onClick={() => setToggle((prev) => !prev)}
+    //     />
+
+    //     <div
+    //       className={`${
+    //         !toggle ? "hidden" : "flex"
+    //       } sidebar absolute right-0 top-20 mx-4 my-2 min-w-[170px] rounded-xl bg-black bg-opacity-95 p-6`}
+    //     >
+    //       <ul className="flex flex-1 list-none flex-col items-start justify-end">
+    //         {navLinks.map((nav, index) => (
+    //           <li
+    //             key={nav.link}
+    //             className={`font-poppins cursor-pointer text-[20px] font-medium ${
+    //               active === nav.name ? "text-white" : "text-dimWhite"
+    //             } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+    //             onClick={() => setActive(nav.name)}
+    //           >
+    //             <a href={`#${nav.link}`}>{nav.name}</a>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
 
