43086bd80f50dac7509126fa6913dc91eae0a27a
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-26 15:09:08 -0700

    chore: Add Google Analytics tracking ID to Vite configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { initGA, logPageView } from "./analytics";
+
+const trackingID = import.meta.env.VITE_GOOGLE_ANALYTICS_TRACKING_ID;
+
+initGA(trackingID);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+logPageView();
