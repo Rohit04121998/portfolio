1ed0f8e434344c94ee22268fa0a44080929c3b87
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-26 21:45:43 -0700

    chore: Update Footer styling and layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,10 +12,10 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <section className="absolute left-0 right-0 mx-20 flex flex-col items-center justify-center overflow-hidden py-6">
+    <section className="absolute left-0 right-0 mx-20 flex flex-col items-center justify-center overflow-hidden py-10">
       <div className="mt-7 w-full border-b border-neutral-700"></div>
-      <div className="flex w-full flex-col items-center justify-between pt-6 md:flex-row">
-        <p className="font-poppins py-6 text-center text-[15px] font-normal leading-[27px] text-white">
+      <div className="flex w-full flex-col items-center justify-between pt-2 md:flex-row">
+        <p className="font-poppins py-6 text-center text-[15px] font-normal leading-[27px] text-white 2xl:text-[18px]">
           Copyright Ⓒ {currentYear} Rohit Veeradhi. All Rights Reserved.
         </p>
 
@@ -23,7 +23,7 @@ export const Footer = () => {
           {socialMedia.map((social, index) => (
             <a
               href={social.link}
-              className={`cursor-pointer object-contain text-3xl lg:text-6xl 2xl:text-2xl ${
+              className={`cursor-pointer object-contain text-3xl 2xl:text-4xl ${
                 index !== socialMedia.length - 1 ? "mr-10" : "mr-0"
               }`}
               target="_blank"
